f03311693485cbc82afff70342b1e20c1e1459cf
Murray Logan <i.obesulus@gmail.com>
2024-10-18 08:49:16 +1000

    Templating to EDA

--- R/20_process_data.R
+++ R/20_process_data.R
@@ -4,6 +4,10 @@ dat <- readRDS(paste0(DATA_PATH, "primary/dat.rds"))
 
 ## Processing steps
 
+## At this point, we should join in a lookup that contains the units
+## for each of the responses and covariates for use in figures and tables.
+## I will ask Katharina to supply a txt file with these units
+
 ## ---- save_data
 saveRDS(dat, paste0(DATA_PATH, "processed/dat.rds"))
 ## ----end
--- R/30_eda.R
+++ R/30_eda.R
@@ -3,11 +3,76 @@
 ## represents a single figure.  The columns would represent:
 ## - Response
 ## - Predictors
-## - subset data
 ## - pointer to file containing the ggplot instructions
 ## - figure caption (as string)
 
 ## ---- eda_load_data
-dat readRDS(paste0(DATA_PATH, "processed/dat.rds"))
-
+dat <- readRDS(paste0(DATA_PATH, "processed/dat.rds"))
 ## ----end
+
+## ---- eda_plot
+eda_plot <- function(dat, response, covariate, pth) {
+  err <- try(
+  {
+    gg <-
+      dat |>
+      ggplot(aes(x = covariate, y = response)) +
+      geom_point() +
+      theme_classic()
+    saveRDS(gg, file = pth)
+  },
+  silent = TRUE
+  )
+  if (inherits(err, "try-error")) {
+    return(NA)
+  } else {
+    return(pth)
+  }
+}
+## ----end
+
+## ---- eda
+eda <- tibble(responses = c("erosionGrazing", "totalErosion")) |>
+  crossing(covariates = c("MA.Perc", "CCA.Perc"))
+eda <- eda |>
+  mutate(
+    n = 1:n(),
+    N = n()
+  ) |>
+  mutate(gg = pmap( ## generate gg plot instructions
+    .l = list(
+      response = responses,
+      covariate = covariates,
+      n = n,
+      N = N
+    ),
+    .f = ~ {
+      response <- ..1
+      covariate <- ..2
+      n <- ..3
+      N <- ..4
+      pth <- paste0(FIGURES_PATH, "eda_", response, "_by_", covariate, ".rds")
+      cat(paste0(n, "/", N, "\tResponse=", response, ", Covariate=", covariate, "\n\t\t(", pth, ")\n"))
+      eda_plot(dat, response = response, covariate = covariate, pth = pth)
+    }
+  )) |>
+  mutate(cap = pmap( ## generate figure caption
+    .l = list(
+      response = responses,
+      covariate = covariates,
+      n = n,
+      N = N
+    ),
+    .f = ~ {
+      response <- ..1
+      covariate <- ..2
+      n <- ..3
+      N <- ..4
+      cat(paste0(n, "/", N, "\tResponse=", response, ", Covariate=", covariate, "\n"))
+      cap <- paste0(response, " against ", covariate, " bla bla bla")
+    }
+  )) 
+## ----end
+
+eda[1, "gg"][[1]][[1]]
+eda[1, "cap"][[1]][[1]]
