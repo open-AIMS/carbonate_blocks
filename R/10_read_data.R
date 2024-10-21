860bfa17dfb31650e9d79a9f0a51e774cb4c77a5
Murray Logan <i.obesulus@gmail.com>
2024-10-21 12:38:29 +1000

    Add loading of units

--- R/10_read_data.R
+++ R/10_read_data.R
@@ -2,3 +2,8 @@
 dat <- read_csv(paste0(RAW_DATA_PATH, "Blocks_eReefs_silt_biota_erosion_metab_2024Oct.csv"))
 saveRDS(dat, paste0(DATA_PATH, "primary/dat.rds"))
 ## ----end
+
+## ---- import units
+units <- read_csv(paste0(RAW_DATA_PATH, "Variable dictionary for Blocks - shortened.csv"))
+saveRDS(dat, paste0(DATA_PATH, "primary/units.rds"))
+## ----end
